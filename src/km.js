3af8da64d525a5ac72e6a497e3f44bbbca02d771
revolutionizeIT <revolutionizeit@gmail.com>
2018-09-13 11:40:08 +0100

    changes to mapping

--- src/km.js
+++ src/km.js
@@ -15,23 +15,23 @@ exports.getContent = (keyword, cb) => {
 	}, (error, response, body) => {
 		
 		//console.log(`HEADERS: ${JSON.stringify(response.headers)}`);
-		console.log('Response Status Code: '+response.statusCode + '.Message: '+response.statusMessage);
+		console.log('Status Code: '+response.statusCode + '.Message: '+response.statusMessage);
 		if(!error && response.statusCode === 200){
 			let content = JSON.parse(body);
-			if(content)
-			
-			response = content.hasOwnProperty('hydra:totalItems') ? `Found ${content['hydra:totalItems']} articles` :
-															'No appropriate FAQ found'
-			let results = content['hydra:member'];
-			if(results){
-				results.array.forEach(element => {
-					let name = $element['hydra:member'][0]['vkm:name'];
-					let description = $element['hydra:member'][0]['vkm:description'];
-					console.log('KM Name: '+name);
-					console.log('KM Desc: '+description);
-					response += name +'-'+ description;
-				});
+			if(content){				
+				response = content.hasOwnProperty('hydra:totalItems') ? `Found ${content['hydra:totalItems']} articles` :
+																'No appropriate FAQ found'
+				let results = content['hydra:member'];
+				if(results){
+					results.array.forEach(element => {
+						let name = $element['hydra:member'][0]['vkm:name'];
+						let description = $element['hydra:member'][0]['vkm:description'];
+						console.log('KM Name: '+name);
+						console.log('KM Desc: '+description);
+						response += name +'-'+ description;
+					});
 
+				}
 			}
 
 			cb(response)
