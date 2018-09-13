6c136b4651168194a1f79ba7150eadbc10caa950
revolutionizeIT <revolutionizeit@gmail.com>
2018-09-13 13:38:12 +0100

    changes to mapping

--- app.js
+++ app.js
@@ -15,9 +15,9 @@ app.use(bodyparser.urlencoded({
 app.use(bodyparser.json());
 
 app.post('/webhook', (req, res) => {
-	const agent = new WebhookClient({ request, response });
-	console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
-	console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
+	const agent = new WebhookClient({ req, res });
+	console.log('Dialogflow Request headers: ' + JSON.stringify(req.headers));
+	console.log('Dialogflow Request body: ' + JSON.stringify(req.body));
 
 	function welcome (agent) {
 		agent.add(`Welcome to my agent!`);
